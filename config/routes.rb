322c05a2b08338f984922893c20ede118b84f97f
webmstk <webmstk@mail.ru>
2015-12-18 16:02:43 +0300

    lesson 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'cards#index'
+
+  resources :cards
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
